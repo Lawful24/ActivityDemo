8e288080285bafe24f3e2ba911a96b42abb5430f
Lawful24 <tarkanyilacko@gmail.com>
2020-09-12 13:52:09 +0100

    Exercise 6: Add the .json file

--- app/src/main/java/com/appplanet/activitydemo/Movie.kt
+++ app/src/main/java/com/appplanet/activitydemo/Movie.kt
@@ -1,3 +1,13 @@
 package com.appplanet.activitydemo
 
-data class Movie (val id: String, val title: String)
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Movie (
+    @Json(name = "id") val id: Int,
+    @Json(name = "release_date") val releaseDate: Int,
+    @Json(name = "title") val title: String,
+    @Json(name = "vote_average") val voteAverage: Double,
+    @Json(name = "total_results") val totalResults: Int
+)
