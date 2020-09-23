4308390cddd079d60fdd2d15db12ad754b3e6612
Lawful24 <tarkanyilacko@gmail.com>
2020-09-23 14:30:50 +0100

    Exercise 7: Add api key to BuildConfig in local gradle.properties

--- app/src/main/java/com/appplanet/activitydemo/MovieDetailsFragment.kt
+++ app/src/main/java/com/appplanet/activitydemo/MovieDetailsFragment.kt
@@ -8,16 +8,6 @@ import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_movie_details.view.textView
 
 class MovieDetailsFragment : Fragment() {
-    companion object {
-        fun getInstance(movie: Movie): MovieDetailsFragment {
-            val args = Bundle()
-            args.putParcelable("movie_object", movie)
-            val fragment = MovieDetailsFragment()
-            fragment.arguments = args
-            return fragment
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,9 +15,19 @@ class MovieDetailsFragment : Fragment() {
     ): View {
         val rootView = inflater.inflate(R.layout.fragment_movie_details, container, false)
 
-        val movie = arguments?.getParcelable<Movie>("movie_object")
+        val movie = arguments?.getParcelable<Movie>(BuildConfig.MOVIE_API_KEY)
         rootView.textView.text = movie?.title
 
         return rootView
+    }
+
+    companion object {
+        fun getInstance(movie: Movie): MovieDetailsFragment {
+            val args = Bundle()
+            args.putParcelable(BuildConfig.MOVIE_API_KEY, movie)
+            val fragment = MovieDetailsFragment()
+            fragment.arguments = args
+            return fragment
+        }
     }
 }
