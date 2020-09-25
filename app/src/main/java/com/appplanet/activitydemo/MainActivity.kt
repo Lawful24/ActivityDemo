1c3eb9c29e7dd520b3dea99a458ed998ab09bb5e
Lawful24 <tarkanyilacko@gmail.com>
2020-09-25 16:45:20 +0100

    Exercise 7: Add hidden test key

--- app/src/main/java/com/appplanet/activitydemo/MainActivity.kt
+++ app/src/main/java/com/appplanet/activitydemo/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Log.d(BuildConfig.MOVIE_API_KEY, "access granted")
+        Log.d("Hidden Key", BuildConfig.MOVIE_API_KEY)
 
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.fragment_container, SearchFragment())
