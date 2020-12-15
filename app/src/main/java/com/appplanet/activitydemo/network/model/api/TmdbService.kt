87c41419da6013266a750cda98395704aadd5bf4
Lawful24 <tarkanyilacko@gmail.com>
2020-12-15 14:05:22 +0100

    Exercise 9: Add network service interface

---
+++ app/src/main/java/com/appplanet/activitydemo/network/model/api/TmdbService.kt
@@ -0,0 +1,15 @@
+package com.appplanet.activitydemo.network.api
+
+import com.appplanet.activitydemo.BuildConfig
+import com.appplanet.activitydemo.network.model.MovieResults
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TmdbService {
+    @GET("search/movie")    // never start the interface definition with a "/"
+
+    fun getMoviesFromQuery(
+        @Query("api_key") apiKey: String = BuildConfig.MOVIE_API_KEY
+    ): Call<MovieResults>
+}
