ab0e7d99c55d48e029efb2e3c1de92f83c69a0a0
Lawful24 <tarkanyilacko@gmail.com>
2021-01-08 00:22:10 +0100

    Exercise 12: Replace Retrofit Callbacks with RxAndroid and RxJava in MovieController

--- app/src/main/java/com/appplanet/activitydemo/network/controller/MovieController.kt
+++ app/src/main/java/com/appplanet/activitydemo/network/controller/MovieController.kt
@@ -3,14 +3,12 @@ package com.appplanet.activitydemo.network.controller
 import android.util.Log
 import com.appplanet.activitydemo.BuildConfig
 import com.appplanet.activitydemo.network.ServerResponseListener
-import com.appplanet.activitydemo.network.model.MovieResponse
 import com.appplanet.activitydemo.network.api.TmdbService
 import com.appplanet.activitydemo.network.model.Movie
 import com.squareup.moshi.Moshi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -30,52 +28,29 @@ class MovieController {
 
     fun searchMovies(query: String, listener: ServerResponseListener<List<Movie>?>) {
         tmdbService.getMoviesFromQuery(BuildConfig.MOVIE_API_KEY, query)
-            .enqueue(object : Callback<MovieResponse> {
-                override fun onResponse(
-                    call: Call<MovieResponse>,
-                    response: Response<MovieResponse>
-                ) {
-                    listener.getResult(response.body()?.results)
-                    // called the interface after the response was handled
-                }
-
-                override fun onFailure(call: Call<MovieResponse>, throwable: Throwable) {
-                    Log.e(MovieController::class.java.simpleName, throwable.message)
-                }
-            })
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                { result -> listener.getResult(result.results) },
+                { throwable -> Log.e(MovieController::class.java.simpleName, throwable.message) })
     }
 
     fun getMostPopularMovies(listener: ServerResponseListener<List<Movie>?>) {
         tmdbService.getMostPopularMovies(BuildConfig.MOVIE_API_KEY)
-            .enqueue(object : Callback<MovieResponse> {
-                override fun onResponse(
-                    call: Call<MovieResponse>,
-                    response: Response<MovieResponse>
-                ) {
-                    listener.getResult(response.body()?.results)
-                    // called the interface after the response was handled
-                }
-
-                override fun onFailure(call: Call<MovieResponse>, throwable: Throwable) {
-                    Log.e(MovieController::class.java.simpleName, throwable.message)
-                }
-            })
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                { response -> listener.getResult(response.results) },
+                { throwable -> Log.e(MovieController::class.java.simpleName, throwable.message) })
     }
 
     fun getMovieById(movieId: Int?, listener: ServerResponseListener<Movie>) {
         tmdbService.getMovieById(movieId, BuildConfig.MOVIE_API_KEY)
-            .enqueue(object : Callback<Movie> {
-                override fun onResponse(
-                    call: Call<Movie>,
-                    response: Response<Movie>
-                ) {
-                    listener.getResult(response.body())
-                    // called the interface after the response was handled
-                }
-
-                override fun onFailure(call: Call<Movie>, throwable: Throwable) {
-                    Log.e(MovieController::class.java.simpleName, throwable.message)
-                }
-            })
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe(
+                { response -> listener.getResult(response) },
+                { throwable -> Log.e(MovieController::class.java.simpleName, throwable.message) }
+            )
     }
 }
