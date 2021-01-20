ca34cec88dc6b377e3f6910ea83de9e5094f0bc1
Lawful24 <tarkanyilacko@gmail.com>
2021-01-20 21:16:48 +0100

    Exercise 12: Move the base URL to a const val

--- app/src/main/java/com/appplanet/activitydemo/network/NetworkSingleton.kt
+++ app/src/main/java/com/appplanet/activitydemo/network/NetworkSingleton.kt
@@ -6,10 +6,12 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
+const val baseUrl = "https://api.themoviedb.org/3/"
+
 object NetworkSingleton {
 
-    val retrofit = Retrofit.Builder()
-        .baseUrl("https://api.themoviedb.org/3/")
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
         .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().build()))
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .client(OkHttpClient())
