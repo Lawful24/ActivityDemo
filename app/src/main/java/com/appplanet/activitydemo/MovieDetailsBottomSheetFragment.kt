260889cf0c77c0e79477015273e7d81b9bc8f291
Lawful <tarkanyilacko@gmail.com>
2021-02-13 12:47:58 +0100

    61, EXC-52: Add error text to bottom sheet, when no email app is installed

--- app/src/main/java/com/appplanet/activitydemo/MovieDetailsBottomSheetFragment.kt
+++ app/src/main/java/com/appplanet/activitydemo/MovieDetailsBottomSheetFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.appplanet.activitydemo.network.model.Movie
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.bottom_sheet_share.noEmailAppText
 import kotlinx.android.synthetic.main.bottom_sheet_share.textShareExternally
 import kotlinx.android.synthetic.main.bottom_sheet_share.textShareViaEmail
 
@@ -46,13 +47,13 @@ class MovieDetailsBottomSheetFragment : BottomSheetDialogFragment() {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) // for the "back" button to navigate back to the app
         }
 
-        if (activity?.packageManager?.queryIntentActivities(shareEmailIntent, 0)?.size != 0) {
-            // todo: set email sub button to gone
+        if (activity?.packageManager?.queryIntentActivities(shareEmailIntent, 0)?.size == 0) {
             textShareViaEmail.setOnClickListener {
                 startActivity(Intent.createChooser(shareEmailIntent, null))
             }
         } else {
-            textShareViaEmail.visibility = View.GONE
+            textShareViaEmail.text = ""
+            noEmailAppText.visibility = View.VISIBLE
         }
     }
 
